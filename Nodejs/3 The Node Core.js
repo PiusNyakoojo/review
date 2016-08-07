3a23c82d04c2dc9e39e925559926bd0192a4aae5
PiusNyakoojo <pnyakoojo.16@gmail.com>
2016-08-06 23:31:57 -0500

    nodejs example server

--- Nodejs/3 The Node Core.js
+++ Nodejs/3 The Node Core.js
@@ -62,4 +62,23 @@
 	of functions that are useful. Things that we could have written ourselves, but were given to us for free by the Nodejs team.
 
 	So we can see that Node.js is a framework as well as a library of code.
+
+	
+	Here's an example web server with Nodejs
+
 */
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'test/plain');
+	res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}/`);
+});
