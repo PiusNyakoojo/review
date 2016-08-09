d544f3c841d4ebbfe55b2f69dca8fefbfdb3d164
PiusNyakoojo <pnyakoojo.16@gmail.com>
2016-08-08 21:49:33 -0500

    Added git commands

---
+++ Git/Commands.js
@@ -0,0 +1,49 @@
+/*
+	
+	// List the URL of the remote repo
+	git remote -v
+
+	
+	// Associate github repo with git repo
+	git remote add origin <URL>
+
+
+	// Change the remote repo association
+	git remote set-url origin <URL>
+
+
+	// Copy a repo
+	git clone <URL>
+	
+
+	// List all branches of a repo on your local machine
+	git branch
+
+
+	// Create a new branch
+	git branch <NAME>
+
+
+	// Create a new branch and immediately switch to it
+	git branch -b <NAME>
+
+
+	// Merge a branch into the master branch
+	git merge <branch>
+
+
+	// Get the most recent changes to a repo
+	git pull origin <branch>
+
+
+	// View what files have been changed
+	git status
+
+
+	// View how much each file has been changed
+	git diff --stat
+
+
+	// View commit history
+	git log
+*/
