7439182d66054c7fee272b27da46975cfac23431
PiusNyakoojo <pnyakoojo.16@gmail.com>
2016-09-29 10:17:56 -0500

    Added more forms info

--- Web Development/Building High Conversion Web Forms/3 Fast Forms.js
+++ Web Development/Building High Conversion Web Forms/3 Fast Forms.js
@@ -11,3 +11,57 @@
 /*
     Empathy is very important. You need to see the world in other people's perspective.
 */
+/*
+    If the form is long, can you save it for later? This is especially important for eCommerce.
+
+    Can you finish the form on a different device? That's anoter essential question to ask when designing a form.
+
+        - How long?
+        - Where can I access the form?
+        - What does your product look like from outside of your company?
+            - Most people think about it from within their organization. "Oh we have these legal requirements, 
+                the tech team can only build this, the design team can do blah blah blah..."
+            - Think from inside out makes sense when you're looking at the bounds of your company. But it doesn't 
+                make sense when you're looking from externally.
+            - A useful to technique to force yourself to think from outside in is to have someone roleplay the 
+                role of a form.
+
+    But remember, FORMS SUCK! KEEP THEM SHORT!
+*/
+/*
+    Roleplaying the role of a form.
+        - Give someone a form, and you act like a human and they are only allowed to respond with what the form 
+            says.
+        - Person: "Hi, I'm luke, I want to see if I'd get along with you."
+        - Form: "First name."
+        - Person: "Uhh.. okay Luke."
+        - Form: "Last name."
+        - Person: "Uhh.. Stevens."
+        - Form: "Gender."
+        - Person: "Male."
+    
+    This name/value pair in a database is not a human interaction friendly environment.
+*/
+/*
+    Complicated passwords may be annoying but they're fairly common. You should always practice security.
+
+    Registration tends to push users away. That is terrible.
+        - Allow checking out as a guest!!!
+
+    Suggesting products is fine.
+
+    Name inputs for shipping addresses is also fine. 
+*/
+/*
+    FASTER
+    SIMPLER
+*/
+/*
+    How can we make forms fast easy and accurate?
+        - Cheap
+            - This is information we need to have in the form. It is critical.
+        - Cut
+            - We actually don't need it.
+        - Postpone
+        - Kill
+*/
