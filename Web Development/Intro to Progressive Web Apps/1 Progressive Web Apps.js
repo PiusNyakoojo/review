0825de3be036facc3d9957e43faf3a376e8f90ef
PiusNyakoojo <pnyakoojo.16@gmail.com>
2016-10-03 01:25:24 -0500

    Added progressive web app info

---
+++ Web Development/Intro to Progressive Web Apps/1 Progressive Web Apps.js
@@ -0,0 +1,29 @@
+/*
+    Progressive Web Apps are experiences that combine the best of a web experience with the best of native 
+    application experiences on mobile devices.
+
+    They give users the kind of 
+        - reliable
+            - When network is poor or there's no network at all.
+        - fast
+            - Scroll and animate smoothly
+        - engaging
+            - Send relevant push notifications to users
+        - integrated
+            - Show up on the home screen as a fullscreen app 
+    
+    experiences that they've come to expect from native applications.
+
+*/
+/*
+    The App Shell model separates the app's content from its data and ensures that the app gets content on the 
+    screen as quickly as possible.
+
+    Then, we'll learn about the service worker and how to use different caching strategies for different needs.
+
+    By the end of the section, our app will use a service worker to cache the app shell and the data so our app 
+    works whether the user is online or offline.
+
+    Finally, we'll learn how to make our web app installable via the web app manifest.
+
+*/
