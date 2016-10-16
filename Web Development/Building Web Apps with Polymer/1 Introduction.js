f5864abcd640980b86e03fd51829e2095363b815
piusnyakoojo <pnyakoojo.16@gmail.com>
2016-10-15 22:24:28 -0500

    Added Polymer info

--- Web Development/Building Web Apps with Polymer/1 Introduction.js
+++ Web Development/Building Web Apps with Polymer/1 Introduction.js
@@ -1,3 +1,18 @@
 /*
   https://polymer-project.org
+
+  https://customelements.io
+
+  Iron icons
+  https://elements.polymer-project.org/elements/iron-icons?view=demo:demo/index.html
+*/
+/*
+  Creating a custom element:
+    - Create an Element
+    - Documentation
+    - Test Scenarios
+    - Optimize
+    - Upload
+    - Demo Page
+    - Publish
 */
---
+++ Web Development/Building Web Apps with Polymer/simple-search/README.md
@@ -0,0 +1,17 @@
+## General Info `<simple-search>`
+
+`<simple-search>` shows a search input area on your page.
+
+To use, insert `<simple-search>` in the body:
+
+  <body>
+    <simple-search></simple-search>
+
+Example:
+
+To show the search
+
+```
+  <body>
+    <simple-search static-open></simple-search>
+```
