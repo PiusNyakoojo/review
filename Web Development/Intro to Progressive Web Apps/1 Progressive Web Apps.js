d035a3166250f2a0e6f1390db0151711242b474e
Pius Nyakoojo <pnyakoojo.16@gmail.com>
2016-10-22 10:49:16 -0500

    Added local storage comparisons

--- Web Development/Intro to Progressive Web Apps/1 Progressive Web Apps.js
+++ Web Development/Intro to Progressive Web Apps/1 Progressive Web Apps.js
@@ -39,3 +39,68 @@
     in the future. We should defer the loading of HTML, Javascript and images as well until after the 1st 
     load experience. 
 */
+/*
+    Server-side rendering introduces a number of challenges for PWAs because the data is already 
+    embedded in the user interface. If the app caches the data, then it can quickly become stale. 
+    As a result, we may get a flash of outdated cotent (instead of flash of unstyled content)
+*/
+/*
+    The easiest, but also the slowest method we can use to get the data we need is through AJAX calls. 
+    Not only does it require an additional HTTP request, but the request won't be fired until the app's 
+    javascript has been parsed and run. 
+*/
+/*
+    The final combination we'll look at is using both server-side rendering and making an ajax request.
+    Instead of pre-rendering the data into the markup, the server injects the data into the application's 
+    javascript. Because the data is kept separate from the user interface, we've eliminated the need for 
+    an additional HTTP request. 
+
+    However, we still need to run some javascript in order to display the data. 
+*/
+/*
+    Local Storage
+
+    Local Storage - Advantages
+        - A simple name-value pair storage mechanism that is easy to use and available on greater than 
+        90% of modern browsers. 
+
+    Local Storage - Disadvantages
+        - However, it's API is synchronous. This means that calls to it are completely blocking! This can 
+        cause significant performance problems. 
+        - Additionally, it's not transactional. So if you try to do 2 writes at the time, you'll end up
+        overriding something. 
+        - Avoid using localStorage. 
+*/
+/*
+    Caches Object
+
+    Caches Objects - Advantages
+        - Easy to use 
+        - Asynchronous
+        - Fast!!
+    
+    Caches Object - Disadvantages
+        - Transactional: when writing to the same data point, the data may ber overriden. 
+        - Not available on all browsers yet. In fact, not a lot of browser support at all. 
+*/
+/*
+    IndexedDB
+
+    IndexedDB - Advantages
+        - FAST!
+        - allows you to store complex data 
+        - Asynchronous 
+        - Transactional
+        - HIGH AVAILABILITY (on more than 70% of modern browsers)
+
+    IndexedDB - Disadvantages
+        - Ugly API. Lots of setup to handle and callbacks to handle. 
+            - However, there are a few good wrapper libraries that will abstract away this ugliness
+            - Mozilla's localForage : which provides a localStorage-like API 
+            - Lovefield (a relational database for the web): provides SQL-like API
+*/
+/*
+    Browsers make no promises that they'll keep data around forever. This locally stored data can 
+    be removed anytime without warning. For that reason, it's important to sync critical data to the 
+    cloud as soon as possible. 
+*/
