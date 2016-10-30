0820d52d6438a9bb5e63ff6004e4e3d4ae8ea184
PiusNyakoojo <pnyakoojo.16@gmail.com>
2016-10-29 19:20:11 -0500

    Added Web performance info

---
+++ Web Development/Web Performance Optimization/1 Getting Up and Running.js
@@ -0,0 +1,25 @@
+/*
+    Critical Rendering Path
+        - The sequence of steps the browser takes to render the page. 
+
+    
+    
+    DOM <------> JavaScript
+     |              |
+    CSSOM <----------
+     |
+    Render Tree
+     |
+    Layout
+     |
+    Render
+*/
+/*
+    PageSpeed Insights is a useful tool in helping you determine the steps you 
+    could take to improve the performance of your website.
+
+    https://developers.google.com/speed/pagespeed/insights/
+*/
+/*
+
+*/
