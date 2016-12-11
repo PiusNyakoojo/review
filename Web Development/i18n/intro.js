368f3e78ffb07cb24cb3af1432ce0798eaf3b887
Pius Nyakoojo <pnyakoojo.16@gmail.com>
2016-12-11 11:33:26 -0600

    Added i18n info

---
+++ Web Development/i18n/intro.js
@@ -0,0 +1,13 @@
+/*
+https://www.youtube.com/watch?v=4fFvrEqncHc
+*/
+/*
+    Changing the language 
+        - Before application initialization
+            - browser details
+            - hostmask
+            - session / request / cookie
+            - 3rd party authentication (SSO, JSP, ...)
+        - On the login page 
+        - On the fly 
+*/
