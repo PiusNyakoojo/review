bb7cf7938e12269f00d582824c12cc172d5e7916
Pius Nyakoojo <pnyakoojo.16@gmail.com>
2016-12-13 16:46:03 -0600

    Added REST best practices

---
+++ Web Development/API Design/REST-best-practices.js
@@ -0,0 +1,14 @@
+/*
+    http://www.vinaysahni.com/best-practices-for-a-pragmatic-restful-api
+*/
+/*
+    - It should use web standards where they make sense
+    - It should be friendly to the developer and be explorable via a browser address bar 
+    - It should be simple, intuitive and consistent to make adpotion not only easy but pleasant
+    - It should provide enough flexibility to power majority of the Enchant UI
+    - It should be efficient, while maintaining balance with the other requirements 
+*/
+/*
+    Result filtering, sorting & searching
+    - It's best to keep the base resource URLs as lean as possible. 
+*/
